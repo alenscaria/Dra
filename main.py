e02be1253b310b937dc94c5be8697b0db97cbab8
Alen Scaria <63664995+alenscaria@users.noreply.github.com>
2022-05-06 11:51:45 +0530

    Update main.py
    insert data into database from webpage

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import math
 from flask import Flask, render_template, url_for, request
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import FileStorage
+import mysql.connector
+from mysql.connector import Error
+
+eidn=1
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -11,6 +15,29 @@ def index():
     heart_rate = request.form['hr']
     qt_int = request.form['qt']
     print(heart_rate+"    "+qt_int)
+
+  #new
+    
+    mydb = mysql.connector.connect(host='localhost',
+                                         database='druglist',
+                                         user='root',
+                                         password='1234')
+      
+    mycursor = mydb.cursor()
+    ff="Doe"
+    gg="Highway 22"
+
+    sql = "INSERT INTO entry(eid,loc,text) VALUES (%s,%s,%s)"
+    val=(eidn,heart_rate,qt_int)
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+    print(mycursor.rowcount, "record inserted.")
+
+
+
+
     f = request.files['file']
     f.save(secure_filename(f.filename))
   return render_template('index.html')
