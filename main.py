1f500d65814ccf54a8192e433fc5ed40b913f043
Alen Scaria <63664995+alenscaria@users.noreply.github.com>
2022-05-06 16:48:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import math
 from flask import Flask, render_template, url_for, request
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import FileStorage
 import mysql.connector
 from mysql.connector import Error
+from datetime import datetime,date
 
-eidn=1
+now=datetime.now()
+cur_time=now.strftime("%H:%M:%S")
+
+eidn=3
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -18,28 +21,27 @@ def index():
 
   #new
     
-    mydb = mysql.connector.connect(host='localhost',
-                                         database='druglist',
-                                         user='root',
-                                         password='1234')
+    mydb = mysql.connector.connect(
+      host='localhost',
+      database='druglist',
+      user='root',
+      password='1234'
+    )
       
     mycursor = mydb.cursor()
-    ff="Doe"
-    gg="Highway 22"
+    screenshot = request.files['file']
+    filename = secure_filename(screenshot.filename)
+    MEDIA_ROOT = "./media/"
+    screenshot.save(MEDIA_ROOT+filename)
 
-    sql = "INSERT INTO entry(eid,loc,text) VALUES (%s,%s,%s)"
-    val=(eidn,heart_rate,qt_int)
+    sql = "INSERT INTO entry3(eid,loc,text,img,dat,timestamp) VALUES (%s,%s,%s,%s,%s,%s)"
+    val=(eidn,heart_rate,qt_int, filename,now,cur_time)
     mycursor.execute(sql, val)
 
     mydb.commit()
 
     print(mycursor.rowcount, "record inserted.")
-
-
-
-
-    f = request.files['file']
-    f.save(secure_filename(f.filename))
+    
   return render_template('index.html')
   
  
